ec54ac6ba75250c1ed016ffbd0c5d05515c71667
Thegick76 <91561500+Thegick76@users.noreply.github.com>
2023-11-11 16:47:05 -0500

    moving animtions

--- Pillow Knight/Assets/Script/Movement.cs
+++ Pillow Knight/Assets/Script/Movement.cs
@@ -11,28 +11,44 @@ public class Movement : MonoBehaviour
 
     public bool grounded = false;
     Rigidbody2D rb;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxisRaw("Horizontal") <0)
+
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
         {
+            transform.localScale = new Vector3(-1, 1, 1);
+            anim.SetBool("Moving", true);
             transform.position += Vector3.left * Time.deltaTime * speed;
+
         }
         else if (Input.GetAxisRaw("Horizontal") > 0)
         {
+
+            transform.localScale = new Vector3(1, 1, 1);
+            anim.SetBool("Moving", true);
             transform.position += Vector3.right * Time.deltaTime * speed;
+
+        }
+        else
+        {
+            anim.SetBool("Moving", false);
         }
 
         if(Input.GetButtonDown("Jump") && grounded)
         {
             grounded = false;
+            anim.SetBool("Grounded", false);
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
     }
@@ -42,6 +58,7 @@ public class Movement : MonoBehaviour
         if(collision.gameObject.CompareTag("Floor"))
         {
             grounded = true;
+            anim.SetBool("Grounded", grounded);
         }
     }
 }
